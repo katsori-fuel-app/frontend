3e9831ef051b1ee68aa98ff65515bc46d756d69f
DenisGo <anywaymaybe1@gmail.com>
2025-01-22 17:06:37 +0400

    feature/reg добавил минимальную авторизацию с редиректом

--- src/app/profile/page.tsx
+++ src/app/profile/page.tsx
@@ -1,9 +1,23 @@
+"use client";
+
 import Link from "next/link";
+import { useEffect, useState } from "react";
+import { getUsers } from "shared/api/service";
 
 export default async function Profile() {
+    const [user, setUser] = useState()
+    
+
+  useEffect(() => {
+    getUsers.then((res) => {
+        console.log(res.data.at(-1));
+      });
+
+  }, []);
     return (
         <div>
             <Link href={`/history`}>История</Link>
+            <h1>привет {}</h1>
         </div>
     )
 };
