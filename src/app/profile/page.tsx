4392067b08ae2d866385ce09f4b6fd9b7fb2a2bf
DenisGo <anywaymaybe1@gmail.com>
2025-01-28 18:33:45 +0400

    feature/profile-page: добавил страницу профиля, настроил вывод изображения, добавил кнопку выйти

--- src/app/profile/page.tsx
+++ src/app/profile/page.tsx
@@ -3,27 +3,42 @@
 import { useEffect, useState } from "react";
 import { userService } from "shared/api/services";
 import { User } from "shared/api/types";
+import "./profilePage.scss";
+import Image from "next/image";
+import { AvatarOfMe } from "../../../public/img/avatar";
 
 export default function Profile() {
-  const [user, setUser] = useState<User>();
-  const [loading, setLoading] = useState(true);
+    const [user, setUser] = useState<User>();
+    const [loading, setLoading] = useState(true);
 
-  useEffect(() => {
-    userService
-      .getAllUser()
-      .then((res) => {
-        setUser(res.data[0]);
-      })
-      .finally(() => setLoading(false));
-  }, []);
+    useEffect(() => {
+        userService
+            .getAllUser()
+            .then((res) => {
+                setUser(res.data[0]);
+            })
+            .finally(() => setLoading(false));
+    }, []);
 
-  if (loading) return <h1>Loading...</h1>;
+    if (loading) return <h1>Loading...</h1>;
 
-  if (!user) return <h1>no user...</h1>;
+    if (!user) return <h1>no user...</h1>;
 
-  return (
-    <div>
-      <h1>привет, {user.login}</h1>
-    </div>
-  );
+    const createdUserDate = new Date(user.createdAt).toISOString();
+    const updatedUserDate = new Date(user.updatedAt).toISOString();
+
+    return (
+        <div className="profile-page">
+            <div className="image-wrapper">
+                <AvatarOfMe />
+            </div>
+
+            <div className="profile-page__fields">
+                <p>login: {user.login}</p>
+                <p>email: {user.email}</p>
+                <p>дата создания профиля: {createdUserDate}</p>
+                <p>последннее обновление было: {updatedUserDate}</p>
+            </div>
+        </div>
+    );
 }
