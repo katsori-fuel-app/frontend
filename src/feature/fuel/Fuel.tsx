d6b4431c776aad79a6ca53761229dea3860f709a
denis <anywaymaybe1@gmail.com>
2025-04-14 20:48:27 +0400

    feature/add-data: добавил данные по заправке

--- src/feature/fuel/Fuel.tsx
+++ src/feature/fuel/Fuel.tsx
@@ -7,34 +7,67 @@ type Setting = {
     fuelCount: number;
     fuelType: number;
     totalMileage: number;
+    fuelCost: number;
     comment?: string;
 }[];
 
 const setting: Setting = [
     {
+        date: '12.02.2025',
+        fuelCount: 25.67,
+        fuelType: 95,
+        totalMileage: 168075,
+        fuelCost: 1500,
+    },
+    {
+        date: '18.02.2025',
+        fuelCount: 17.11,
+        fuelType: 95,
+        totalMileage: 168250, // в данных было 168 только
+        fuelCost: 1000,
+    },
+    {
+        date: '02.03.2025',
+        fuelCount: 25.61,
+        fuelType: 95,
+        totalMileage: 168447,
+        fuelCost: 1523,
+    },
+    {
         date: '12.03.2025',
         fuelCount: 25.61,
         fuelType: 95,
         totalMileage: 168711,
+        fuelCost: 1523,
     },
     {
         date: '19.03.2025',
         fuelCount: 25.61,
         fuelType: 95,
         totalMileage: 168933,
+        fuelCost: 1500,
     },
     {
         date: '29.03.2025',
         fuelCount: 25.61,
         fuelType: 95,
         totalMileage: 169159,
+        fuelCost: 1500,
     },
     {
         date: '06.04.2025',
         fuelCount: 25.61,
         fuelType: 95,
-        totalMileage: 169458,
+        totalMileage: 169438,
+        fuelCost: 1500,
         comment: 'машина дёргается после заправки иногда',
+    },
+    {
+        date: '11.04.2025',
+        fuelCount: 25.51,
+        fuelType: 95,
+        totalMileage: 169709,
+        fuelCost: 1523,
     },
 ];
 
@@ -80,7 +113,7 @@ export const Fuel = () => {
                     const fuelConsumption = getFuelConsumption(
                         setting.fuelCount,
                         consumedMileage,
-                        i
+                        i,
                     );
 
                     return (
