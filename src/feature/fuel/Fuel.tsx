b4ac8692962b14af9f6dd3a50bd9c66a66327458
lovelybullshit <anywaymaybe1@gmail.com>
2025-05-20 18:49:05 +0400

    fix/table: вернул подсчет данных обрабтно

--- src/feature/fuel/Fuel.tsx
+++ src/feature/fuel/Fuel.tsx
@@ -5,15 +5,6 @@ import { FuelTable } from './fuel-table';
 import './fuel.scss';
 import { fueldMockData } from './mock-data/TableData';
 
-// type Setting = {
-//     date: string;
-//     fuelCount: number;
-//     fuelType: number;
-//     totalMileage: number;
-//     fuelCost: number;
-//     comment?: string;
-// }[];
-
 const forecastedValue: number = fueldMockData.reduce((acc, _, i, settingArray) => {
     const consumedMileage =
         i === 0 ? 0 : settingArray[i].totalMileage - settingArray[i - 1].totalMileage;
@@ -26,26 +17,6 @@ const forecastedValue: number = fueldMockData.reduce((acc, _, i, settingArray) =
 
     return acc;
 }, 0);
-
-/**
- * Получает пройденное расстояние на 1 баке бенза.
- */
-// const getConsumedMileage = (arr: Setting, i: number): number => {
-//     const consumedMileage = i === 0 ? 0 : Math.floor(arr[i].totalMileage - arr[i - 1].totalMileage);
-
-//     return consumedMileage;
-// };
-
-/**
- * Получает расход топлива 10л/100км.
- */
-// const getFuelConsumption = (fuelCount: number, consumedMileage: number, i: number): string => {
-//     if (i === 0) return 'нет данных';
-
-//     const res = Math.floor((fuelCount / consumedMileage) * 100);
-
-//     return `${res}л`;
-// };
 
 export const Fuel = () => {
     return (
