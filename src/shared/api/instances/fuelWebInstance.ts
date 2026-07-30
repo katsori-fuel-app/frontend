9b769df26975b000169af2690f6fbd8430b98054
denis go <gorbunov.d@optimax.dev>
2026-07-30 18:00:22 +0400

    use remote backend

--- src/shared/api/instances/fuelWebInstance.ts
+++ src/shared/api/instances/fuelWebInstance.ts
@@ -1,7 +1,11 @@
 import axios from 'axios';
 
+const localhost = 'http://localhost:4000/'
+const vdsServer = 'http://91.107.120.104:3000/'
+const baseURL = process.env.NEXT_PUBLIC_IS_REMOTE_BACK === 'remote' ? vdsServer : localhost;
+
 export const fuelWebInstance = axios.create({
-    baseURL: 'http://localhost:4000/',
+    baseURL,
     headers: {
         'Content-Type': 'application/json',
     },
