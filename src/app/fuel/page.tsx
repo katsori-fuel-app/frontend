c95cbd1014415d66b7242f244f62359de7951c46
denis go <gorbunov.d@optimax.dev>
2026-09-23 17:14:53 +0400

    fix/disable-fuel-count/91: undisable input

--- src/app/fuel/page.tsx
+++ src/app/fuel/page.tsx
@@ -1,12 +1,7 @@
 'use client';
 
 import { Fuel } from 'feature/fuel/Fuel';
-import { FirstRefuel } from 'feature/fuel/ui/first-refuel/FirstRefuel';
 
 export default function FuelPage() {
-    const isFirstRefuel = true;
-
-    return (
-        isFirstRefuel ? <FirstRefuel /> : <Fuel />
-    );
+    return <Fuel />
 }
